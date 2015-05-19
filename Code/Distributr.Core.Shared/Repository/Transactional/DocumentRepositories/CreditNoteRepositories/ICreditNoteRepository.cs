8a69f294571e921278d7dec6111605d185ac5391
nutshellit <anthony.moloney@gmail.com>
2015-05-19 14:22:11 +0100

    Credit Note Repository - Do not expose save on contract

--- Code/Distributr.Core.Shared/Repository/Transactional/DocumentRepositories/CreditNoteRepositories/ICreditNoteRepository.cs
+++ Code/Distributr.Core.Shared/Repository/Transactional/DocumentRepositories/CreditNoteRepositories/ICreditNoteRepository.cs
@@ -6,7 +6,7 @@ using Distributr.Core.Domain.Transactional.DocumentEntities;
 
 namespace Distributr.Core.Repository.Transactional.DocumentRepositories.CreditNoteRepositories
 {
-    public interface ICreditNoteRepository : IDocumentRepository<CreditNote>, IDocumentRepositorySaveable<CreditNote>
+    public interface ICreditNoteRepository : IDocumentRepository<CreditNote>//, IDocumentRepositorySaveable<CreditNote>
     {
         List<CreditNote> GetCreditNotesByInvoiceId(Guid invoiceId = new Guid());
     }
