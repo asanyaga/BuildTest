86574a2239053c6840576c0c97e115106f6b6027
mpkelly <kikemelly@gmail.com>
2015-06-02 20:22:52 +0100

    Add Distributr.Mobile folder

---
+++ Code/Distributr.Mobile/Distributr.Core.Shared/CommandHandler/ActivityDocumentHandlers/IAddActivityInfectionLineItemCommandHandler.cs
@@ -0,0 +1,10 @@
+using Distributr.Core.Commands;
+using Distributr.Core.Commands.ActivityDocumentCommands;
+
+namespace Distributr.Core.CommandHandler.ActivityDocumentHandlers
+{
+    public interface IAddActivityInfectionLineItemCommandHandler : ICommandHandler<AddActivityInfectionLineItemCommand>
+    {
+
+    }
+}
---
+++ Code/Distributr.Mobile/Distributr.Core.Shared/CommandHandler/DocumentCommandHandlers/AdjustmentNotes/IConfirmInventoryAdjustmentNoteCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Distributr.Core.Commands;
+using Distributr.Core.Commands.DocumentCommands.AdjustmentNotes;
+
+namespace Distributr.Core.CommandHandler.DocumentCommandHandlers.AdjustmentNotes
+{
+    public interface IConfirmInventoryAdjustmentNoteCommandHandler : ICommandHandler<ConfirmInventoryAdjustmentNoteCommand>
+    {
+    }
+}
