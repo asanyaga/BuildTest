16e1fb437e4233443d81e46408fd300dad664173
Kapsrepo <michael.kaboro@gmail.com>
2015-06-04 16:53:46 +0300

    Test Pull after shortening of File Paths

--- Code/Agrimanagr.WPF.UI/Properties/AssemblyInfo.cs
+++ Code/Agrimanagr.WPF.UI/Properties/AssemblyInfo.cs
@@ -53,5 +53,6 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
+
 [assembly: AssemblyVersion("3.0.0.2")]
 [assembly: AssemblyFileVersion("3.0.0.2")]
