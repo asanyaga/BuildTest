ea6a37eff07df396a440a10b2c11ba85af19fb11
gmwasi <gmwasi@users.noreply.github.com>
2015-07-03 16:21:09 +0300

    Added Farm Activity reports

---
+++ Code/Reports/Agrimanagr/default/scripts/sp_A_InfectionDetailsSummary.sql
@@ -0,0 +1,59 @@
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'sp_A_InfectionDetailsSummary')
+   exec('CREATE PROCEDURE [sp_A_InfectionDetailsSummary] AS BEGIN SET NOCOUNT ON; END')
+GO
+Alter PROCEDURE sp_A_InfectionDetailsSummary
+(
+@StartDate AS DATE,
+@EndDate AS DATE,
+@HubId AS NVARCHAR(50),
+@RouteId AS NVARCHAR(50),
+@CentreId AS NVARCHAR(50),
+@FarmerId AS NVARCHAR(50),
+@FarmId AS NVARCHAR(50),
+@ActivityId AS NVARCHAR(50),
+@ClerkId AS NVARCHAR(50)
+)
+
+AS
+
+if  @HubId='ALL'  begin set @HubId='%' end
+if  @RouteId='ALL'  begin set @RouteId='%' end
+if  @CentreId='ALL'  begin set @CentreId='%' end
+if  @FarmerId='ALL'  begin set @FarmerId='%' end
+if  @FarmId='ALL'  begin set @FarmId='%' end
+if  @ActivityId='ALL'  begin set @ActivityId='%' end
+if  @ClerkId='ALL'  begin set @ClerkId='%' end
+
+SELECT	dbo.tblActivityDocument.Id AS ActivityId,
+		dbo.tblActivityDocument.ActivityReference,
+		dbo.tblActivityType.Name AS ActivityName, 
+		(dbo.tblCommodityOwner.FirstName + ' ' + dbo.tblCommodityOwner.Surname) as FarmerName,
+		dbo.tblInfection.Name AS Infection,
+		dbo.tblActivityInfectionLineItem.InfectionRate,
+		dbo.tblCommodityProducer.Name AS Farm,
+		dbo.tblActivityDocument.ActivityDate AS TimeStamp
+
+FROM	dbo.tblActivityDocument 
+		INNER JOIN dbo.tblActivityType ON dbo.tblActivityDocument.ActivityTypeId = dbo.tblActivityType.Id 
+		INNER JOIN dbo.tblActivityInfectionLineItem ON dbo.tblActivityDocument.Id =dbo.tblActivityInfectionLineItem.ActivityId
+		INNER JOIN dbo.tblInfection ON dbo.tblActivityInfectionLineItem.InfectionId = dbo.tblInfection.id
+		INNER JOIN dbo.tblCostCentre ON dbo.tblCostCentre.ParentCostCentreId = dbo.tblActivityDocument.hubId
+		INNER JOIN dbo.tblCommodityOwner ON dbo.tblCostCentre.Id = dbo.tblCommodityOwner.CostCentreId
+		INNER JOIN dbo.tblCommodityProducer ON dbo.tblActivityDocument.CommodityProducerId = dbo.tblCommodityProducer.Id
+		INNER JOIN dbo.tblUsers ON dbo.tblActivityDocument.ClerkId = dbo.tblUsers.CostCenterId
+
+WHERE	tblCostCentre.CostCentreType2 =1
+		AND(CONVERT(VARCHAR(26),tblActivityDocument.ActivityDate,23)  BETWEEN @startDate AND @endDate)   
+        AND(CONVERT(NVARCHAR(50),dbo.tblCostCentre.Id) LIKE ISNULL(@hubId, N'%'))             
+        AND(CONVERT(NVARCHAR(50),dbo.tblActivityDocument.RouteID) LIKE ISNULL(@routeId, N'%'))  
+        AND(CONVERT(NVARCHAR(50),dbo.tblActivityDocument.CentreId) LIKE ISNULL(@centreId, N'%'))
+        AND(CONVERT(NVARCHAR(50),dbo.tblCommodityOwner.Id) LIKE ISNULL(@farmerId, N'%'))
+		AND(CONVERT(NVARCHAR(50),dbo.tblCommodityProducer.Id) LIKE ISNULL(@farmId, N'%'))
+		AND(CONVERT(NVARCHAR(50),dbo.tblActivityDocument.Id) LIKE ISNULL(@ActivityId, N'%'))
+		AND(CONVERT(NVARCHAR(50),dbo.tblUsers.Id) LIKE ISNULL(@ClerkId, N'%'))
+
+ORDER BY tblActivityDocument.ActivityDate DESC
+
+
+-- EXEC sp_A_InfectionDetailsSummary @StartDate='2015-01-01',@EndDate='2015-07-15',@HubId='ALL',@RouteId='ALL',@CentreId='ALL',@FarmerId='ALL',@FarmId='ALL',@ActivityId='ALL',@ClerkId='ALL'
+					 
